c1b7f6090b18ec45f0b09662cbb0b38de9f7fb4b
Emir Takayama <ngemir@gmail.com>
2022-09-20 18:19:05 -0300

    Melhorar leitura dos dias da semana e Enums

---
+++ app/enums/dias-da-semana.ts
@@ -0,0 +1,11 @@
+export enum DiasDaSemana {
+    // enum coloca números automaticamente para cada atributo e é readonly por padrão
+    // Mas é mais confiável declarar manualmente.
+    DOMINGO = 0,
+    SEGUNDA = 1,
+    TERCA = 2,
+    QUARTA = 3,
+    QUINTA = 4,
+    SEXTA = 5,
+    SABADO = 6
+}
---
+++ dist/js/enums/dias-da-semana.js
@@ -0,0 +1,12 @@
+export var DiasDaSemana;
+(function (DiasDaSemana) {
+    // enum coloca números automaticamente para cada atributo e é readonly por padrão
+    // Mas é mais confiável declarar manualmente.
+    DiasDaSemana[DiasDaSemana["DOMINGO"] = 0] = "DOMINGO";
+    DiasDaSemana[DiasDaSemana["SEGUNDA"] = 1] = "SEGUNDA";
+    DiasDaSemana[DiasDaSemana["TERCA"] = 2] = "TERCA";
+    DiasDaSemana[DiasDaSemana["QUARTA"] = 3] = "QUARTA";
+    DiasDaSemana[DiasDaSemana["QUINTA"] = 4] = "QUINTA";
+    DiasDaSemana[DiasDaSemana["SEXTA"] = 5] = "SEXTA";
+    DiasDaSemana[DiasDaSemana["SABADO"] = 6] = "SABADO";
+})(DiasDaSemana || (DiasDaSemana = {}));
