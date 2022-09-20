20332b43185980d65d6a55e365fd75b3f9431fd5
Emir Takayama <ngemir@gmail.com>
2022-09-20 17:37:35 -0300

    Generic, Classe  e método Abstrato

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,7 +1,7 @@
 import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
-export class NegociacoesView extends View {    
+export class NegociacoesView extends View<Negociacoes> {    
 
     template(model: Negociacoes): string {
         return `
@@ -26,10 +26,5 @@ export class NegociacoesView extends View {
             </tbody>
         </table>
         `;
-    }
-
-    update(model: Negociacoes): void {
-        const template = this.template(model);
-        this.elemento.innerHTML = template;
     }
 }
